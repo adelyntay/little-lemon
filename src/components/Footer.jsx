c5c8a2f3cf1e8b76271cc56e346197cbe1c77d0e
ade-t <adelyn.tjy@gmail.com>
2023-11-10 02:32:43 +0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,16 @@ import logo2 from '../images/logo2.png';
 function Footer() {
     return(
         <footer>
-            <img src={logo2} alt="Footer logo" width="50vh"/>
+            <div className="footer-logo">
+                <img src={logo2} alt="Footer logo" width="50vh"/>
+            </div>
+            <div className="footer-address">
+                <p>Opening Hours:</p>
+                <p className="timing">Closed every Monday<br/>
+                Tues - Fri: 9am - 1pm<br/>
+                Sat - Sun: 9am - 2pm<br/>
+                </p>
+            </div>
         </footer>
     )
 
